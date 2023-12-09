76c412ab3b79604745f87f22b8ab5af71f457f8e
Affi-Amine <affi.amin.work@gmail.com>
2023-12-09 09:20:07 +0000

    setting up the searching with a filtered array

--- src/App.js
+++ src/App.js
@@ -6,7 +6,18 @@ import AppointmentInfo from "./components/AppointmentInfo";
 
 function App() {
   let [appointmentList, setAppointmentList] = useState([]);
+  let [query, setQuery] = useState("");
   
+  const filteredAppointments = appointmentList.filter(
+    item => {
+      return (
+        item.petName.toLowerCase().includes(query.toLowerCase()) ||
+        item.ownerName.toLowerCase().includes(query.toLowerCase()) ||
+        item.aptNotes.toLowerCase().includes(query.toLowerCase()) 
+      )
+    }
+  )
+
   const fetchData = useCallback(() => {
     fetch("./data.json")
       .then(response => response.json())
@@ -23,10 +34,12 @@ function App() {
     <div className="App container mx-auto mt-3 font-thin">
       <h1 className="text-5xl">
         <BiCalendar className="mr-2 inline-block text-red-400 align-top" />Your Appointments</h1>
-        <Search />
+        <Search query = {query}
+          onQueryChange={myQuery => setQuery(myQuery)}
+        />
         <AddAppointment />
         <ul className="divide-y divide-gray-200">
-        { appointmentList
+        { filteredAppointments
           .map(appointment => (
             <AppointmentInfo 
               key = { appointment.key }
